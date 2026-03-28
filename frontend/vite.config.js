c62f347d57014bfcb2f3c59b228b89cbdebba11f
Adam DeJans Jr. <adam.dejans@gmail.com>
2026-03-28 16:49:07 -0400

    Add files via upload

---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/auth': 'http://localhost:8000',
+      '/rooms': 'http://localhost:8000',
+      '/rounds': 'http://localhost:8000',
+      '/policies': 'http://localhost:8000',
+      '/results': 'http://localhost:8000',
+      '/leaderboard': 'http://localhost:8000',
+    },
+  },
+})
